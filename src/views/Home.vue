76471159a37b1e2f98fb9014a44dad1985f8d0c6
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-02 17:39:23 +0200

    [Home] Created component

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <img src="../assets/logo.png" alt="Vue logo">
+        <Welcome msg="Bienvenid@ a Sudokers App"/>
+    </div>
+</template>
+
+<script>
+  import Welcome from "../components/Welcome.vue";
+
+  export default {
+    name: "Home",
+    components: {
+      Welcome
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
