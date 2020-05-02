50814ded822c98dc8e515774972537ed19e6cc81
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-02 20:37:53 +0200

    [Home] Redirect to Login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,18 @@
 
 <script>
   import Welcome from "../components/Welcome.vue";
+  import router from "../router";
 
   export default {
     name: "Home",
     components: {
       Welcome
+    },
+    created() {
+      setTimeout(function () {
+        console.log('router: ', router);
+        router.push({ path: "/login" });
+      }, 3000);
     }
   }
 </script>
