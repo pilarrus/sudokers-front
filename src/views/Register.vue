5706ccce3d4d9db822dcd6933bc0e6c6db9c2263
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-02 17:34:18 +0200

    [Register] Created component

---
+++ src/views/Register.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="register">
+        <div class="register-header">
+            <router-link to="/login">Login</router-link>
+            <span class="separator">|</span>
+            <p>Register</p>
+        </div>
+        <h1>Hello from Register</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Register"
+  }
+</script>
+
+<style scoped>
+    #register {
+        background: #42b983;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px;
+    }
+    .register-header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        font-weight: bold;
+        width: 50%;
+        max-width: 250px;
+    }
+
+    .separator {
+        margin: 5px;
+    }
+</style>
