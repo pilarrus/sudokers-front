c10022a0b0420ac587edaa1f100d2827c14c631a
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-02 20:59:55 +0200

    [NotFound] Created component

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
+import NotFound from "./views/NotFound";
 
 Vue.use(Router);
 
@@ -22,6 +23,11 @@ export default new Router({
       path: "/register",
       name: "register",
       component: Register
+    },
+    {
+      path: "*",
+      name: "notFound",
+      component: NotFound
     }
   ]
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>La página no existe</h1>
+</template>
+
+<script>
+  // import router from "../router";
+
+  export default {
+    name: "NotFound"
+  }
+</script>
+
+<style scoped>
+
+</style>
