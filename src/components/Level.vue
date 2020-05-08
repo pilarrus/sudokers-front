4de9cbb214219dfdee5c98fedd0d49a93238e29e
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-08 18:48:53 +0200

    [Level] Created component

---
+++ src/components/Level.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="level pointer">{{ level }}</div>
+</template>
+
+<script>
+  export default {
+    name: "Level",
+    props: {
+      level: String
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
