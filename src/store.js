3efb73e90d2da358be67e886675408c143b70436
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-10 18:57:53 +0200

    [store.js] Added action and setAction

--- src/store.js
+++ src/store.js
@@ -7,11 +7,13 @@ export default new Vuex.Store({
   state: {
     user: {},
     level: {},
+    action: "",
     sudoku: {}
   },
   mutations: {
     setUser: (state, user) => (state.user = user),
     setLevel: (state, levelChoise) => (state.level = levelChoise),
+    setAction: (state, action) => (state.action = action),
     setSudoku: (state, sudoku) => (state.sudoku = sudoku)
   }
 });
