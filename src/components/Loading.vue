eced2dc90c9d2b6906bb8639500d5badbc649ab3
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-10 17:20:20 +0200

    [Loading] Created component

---
+++ src/components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="loader"></div>
+</template>
+
+<script>
+  export default {
+    name: "Loading"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
     <div class="home">
         <img id="logo" src="../assets/logo.png" alt="Vue logo">
         <Welcome msg="Bienvenid@ a Sudokers App"/>
+        <Loading />
     </div>
 </template>
 
 <script>
   import Welcome from "../components/Welcome.vue";
   import router from "../router";
+  import Loading from "../components/Loading";
 
   export default {
     name: "Home",
     components: {
-      Welcome
+      Welcome,
+      Loading
     },
     created() {
       setTimeout(function () {
