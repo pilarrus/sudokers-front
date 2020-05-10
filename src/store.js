fb717077bba3ec8a676ebd91731ba08e38fa91d8
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-10 17:42:14 +0200

    [store.js] Added user and setUser

--- src/store.js
+++ src/store.js
@@ -5,9 +5,11 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {},
     level: {}
   },
   mutations: {
+    setUser: (state, user) => (state.user = user),
     setLevel: (state, levelChoise) => (state.level = levelChoise)
   }
 });
