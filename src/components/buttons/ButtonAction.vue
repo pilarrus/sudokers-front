f2bd1aefc572591cb567bc28725c41b40a3637a1
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-16 18:32:57 +0200

    [ButtonAction] Created component

---
+++ src/components/buttons/ButtonAction.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="button button-action pointer" v-on:click="setActive">{{action}}</button>
+</template>
+
+<script>
+  export default {
+    name: "ButtonAction",
+    props: {
+      action: String
+    },
+    data() {
+      return {
+        active: false
+      }
+    },
+    methods: {
+      setActive: function () {
+        this.active = !this.active;
+        console.log("active", this.active);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
