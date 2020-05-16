0fd8fadce3336381032db78f9339b03d56f7748f
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-16 17:58:44 +0200

    [ButtonNumber] Created component

---
+++ src/components/buttons/ButtonNumber.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="button button-number pointer" v-on:click="counter += 1" :disabled="counter >= 9">
+        <span class="button-number-box">{{number}}</span>
+        <span class="button-number-count">{{counter}}</span>
+    </button>
+</template>
+
+<script>
+  export default {
+    name: "Number",
+    props: {
+      number: Number
+    },
+    data() {
+      return {
+        counter: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
