a9763ee181956f1e8ea3626f8381211c99ee8b44
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-16 18:33:37 +0200

    [Sudoku] Added ButtonAction component

--- src/views/Sudoku.vue
+++ src/views/Sudoku.vue
@@ -15,27 +15,39 @@
             </div>
         </div>
 
-        <div class="sudoku-bar-actions">Actions</div>
+        <div class="sudoku-actions">
+            <div class="sudoku-actions-container" v-for="action in actions" :key="action" v-on:click="setSelectedAction(action)">
+                <ButtonAction v-bind:action="action"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-  import ButtonNumber from "../components/buttons/ButtonNumber"
+  import ButtonNumber from "../components/buttons/ButtonNumber";
+  import ButtonAction from "../components/buttons/ButtonAction";
 
   export default {
     name: "Sudoku",
     components: {
-      ButtonNumber
+      ButtonNumber,
+      ButtonAction
     },
     data() {
       return {
-        selectedNumber: 0
+        selectedNumber: 0,
+        actions: ["A","B","C","D","E","F","G","H"],
+        selectedAction: ""
       }
     },
     methods: {
       setSelectedNumber: function (number) {
         this.selectedNumber = number;
         console.log("selectedNumber: ", this.selectedNumber);
+      },
+      setSelectedAction: function (action) {
+        this.selectedAction = action;
+        console.log("selectedAction: ", this.selectedAction);
       }
     }
   }
