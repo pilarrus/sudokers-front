f2b2248c0eec6e1b87992d8b70d8d7ed796fa01b
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-18 18:08:05 +0200

    [Cell] Added numbers prop. Deleted position and writable props, data, methods, created and updated

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -2,7 +2,7 @@
     <div v-if="action === 'paint'" class="cell cell-number">{{number}}</div>
 
     <div v-else-if="action === 'mark'" class="cell cell-grid">
-        <span v-for="n in numbers" :key="n" class="cell-grid-item" :class="['cell-grid-item-' + number]">{{number}}</span>
+        <span v-for="n in numbers" :key="n" class="cell-grid-item" :class="['cell-grid-item-' + n]">{{n}}</span>
     </div>
 
     <div v-else-if="action === '' || action === 'delete'" class="cell"></div>
@@ -14,52 +14,7 @@
     props: {
       action: String,
       number: Number,
-      position: Object,
-      writable: Boolean
-    },
-    data() {
-      return {
-        numbers: []
-      }
-    },
-    methods: {
-      addNumberToNumbers: function () {
-        this.numbers.push(this.number);
-      },
-      deleteNumberToNumbers: function (index) {
-        this.numbers.splice(index,1);
-      },
-      compare: function (x, y) {
-        if (x < y) return -1;
-        else if (x > y) return 1;
-        return 0;
-      },
-      sortNumbers: function (compare) {
-        this.numbers.sort(compare);
-      },
-      markAction: function () {
-        if (this.numbers.length === 0) {
-          this.addNumberToNumbers();
-        } else {
-          const index = this.numbers.findIndex(element => element === this.number);
-          if (index < 0) {
-            this.addNumberToNumbers();
-          } else {
-            this.deleteNumberToNumbers(index);
-          }
-          this.sortNumbers(this.compare);
-        }
-      }
-    },
-    created() {
-      if (this.action === 'mark') {
-        this.markAction();
-      }
-    },
-    updated() {
-      if (this.action === 'mark') {
-        this.markAction();
-      }
+      numbers: Array
     }
   }
 </script>
