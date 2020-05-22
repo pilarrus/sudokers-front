db94c916cb34f1d86b1f24ad518345a726b72807
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-22 20:44:52 +0200

    [ActionBar] Created component

---
+++ src/components/ActionBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="sudoku-actions">
+        <div class="sudoku-actions-container" v-for="action in actions" :key="action"
+             v-on:click="$emit('send-selected-action', action)">
+            <ButtonAction :action="action"/>
+        </div>
+    </div>
+</template>
+
+<script>
+  import ButtonAction from "./buttons/ButtonAction";
+
+  export default {
+    name: "ActionBar",
+    components: {
+      ButtonAction
+    },
+    data() {
+      return {
+        actions: ["delete", "mark", "paint"]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
