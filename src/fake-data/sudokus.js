2588285ab2537b798faca827f3056684459237d8
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-22 21:14:11 +0200

    Modified fake-data sudokus

--- src/fake-data/sudokus.js
+++ src/fake-data/sudokus.js
@@ -4,20 +4,731 @@ export default [
     cells: [
       {
         position: {
-          row: 7,
-          column: 4
+          row: 1,
+          column: 1
         },
         writable: true,
-        number: 9,
-        grid: [1, 4]
+        number: 0,
+        grid: []
       },
       {
         position: {
           row: 1,
           column: 2
         },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 3
+        },
+        writable: false,
+        number: 6,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 4
+        },
+        writable: false,
+        number: 7,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 5
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 6
+        },
         writable: false,
         number: 8,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 7
+        },
+        writable: false,
+        number: 1,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 8
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 1,
+          column: 9
+        },
+        writable: false,
+        number: 4,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 1
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 2
+        },
+        writable: false,
+        number: 7,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 3
+        },
+        writable: false,
+        number: 4,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 4
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 5
+        },
+        writable: false,
+        number: 5,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 6
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 7
+        },
+        writable: false,
+        number: 2,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 8
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 2,
+          column: 9
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 1
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 2
+        },
+        writable: false,
+        number: 1,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 3
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 4
+        },
+        writable: false,
+        number: 2,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 5
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 6
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 7
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 8
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 3,
+          column: 9
+        },
+        writable: false,
+        number: 6,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 1
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 2
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 3
+        },
+        writable: false,
+        number: 2,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 4
+        },
+        writable: false,
+        number: 8,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 5
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 6
+        },
+        writable: false,
+        number: 9,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 7
+        },
+        writable: false,
+        number: 7,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 8
+        },
+        writable: false,
+        number: 4,
+        grid: []
+      },
+      {
+        position: {
+          row: 4,
+          column: 9
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 1
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 2
+        },
+        writable: false,
+        number: 4,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 3
+        },
+        writable: false,
+        number: 8,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 4
+        },
+        writable: false,
+        number: 6,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 5
+        },
+        writable: false,
+        number: 3,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 6
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 7
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 8
+        },
+        writable: false,
+        number: 9,
+        grid: []
+      },
+      {
+        position: {
+          row: 5,
+          column: 9
+        },
+        writable: false,
+        number: 2,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 1
+        },
+        writable: false,
+        number: 3,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 2
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 3
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 4
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 5
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 6
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 7
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 8
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 6,
+          column: 9
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 1
+        },
+        writable: false,
+        number: 7,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 2
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 3
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 4
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 5
+        },
+        writable: false,
+        number: 6,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 6
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 7
+        },
+        writable: false,
+        number: 3,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 8
+        },
+        writable: false,
+        number: 8,
+        grid: []
+      },
+      {
+        position: {
+          row: 7,
+          column: 9
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 1
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 2
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 3
+        },
+        writable: false,
+        number: 9,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 4
+        },
+        writable: false,
+        number: 3,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 5
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 6
+        },
+        writable: false,
+        number: 1,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 7
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 8
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 8,
+          column: 9
+        },
+        writable: false,
+        number: 7,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 1
+        },
+        writable: false,
+        number: 6,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 2
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 3
+        },
+        writable: false,
+        number: 3,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 4
+        },
+        writable: false,
+        number: 5,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 5
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 6
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 7
+        },
+        writable: true,
+        number: 0,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 8
+        },
+        writable: false,
+        number: 1,
+        grid: []
+      },
+      {
+        position: {
+          row: 9,
+          column: 9
+        },
+        writable: true,
+        number: 0,
         grid: []
       }
     ],
