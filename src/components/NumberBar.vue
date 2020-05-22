709ff15dcc9dc469ab9c30f292b2d015c2e89f57
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-05-22 20:44:19 +0200

    [NumberBar] Created component

---
+++ src/components/NumberBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sudoku-numbers">
+        <div class="sudoku-numbers-container" v-for="n in 9" :key="'number' + n" v-on:click="$emit('send-selected-number', n)">
+            <ButtonNumber :number="n"/>
+        </div>
+    </div>
+</template>
+
+<script>
+  import ButtonNumber from "./buttons/ButtonNumber";
+
+  export default {
+    name: "ButtonBar",
+    components: {
+      ButtonNumber
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
