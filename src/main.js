2c6047990e35f98d066a6f55eeb4590c63ffb2b6
pilar-rus <pilar.rus@schaman.com>
2020-05-26 19:30:51 +0200

    Added fortawesome dependencies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,35 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheck,
+  faEllipsisV,
+  faEraser,
+  faInfoCircle,
+  faPencilAlt,
+  faQuestion,
+  faRedo,
+  faThumbtack,
+  faUndo,
+  faUserTimes
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faCheck,
+  faEllipsisV,
+  faEraser,
+  faInfoCircle,
+  faPencilAlt,
+  faQuestion,
+  faRedo,
+  faThumbtack,
+  faUndo,
+  faUserTimes
+);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
