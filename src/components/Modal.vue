9cb3990a7228ca08f2233f96d489e64952fce7e0
pilar-rus <pilar.rus@schaman.com>
2020-06-03 18:02:18 +0200

    [Modal] Created component

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+
+                    <slot></slot>
+
+                    <button class="button" @click="$emit('close')">OK</button>
+
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+  export default {
+    name: "Modal"
+  }
+</script>
+
+<style scoped>
+
+</style>
