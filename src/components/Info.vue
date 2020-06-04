9a12c8f4bc9ccf2fe07cb2657f6d731f3660e120
pilar-rus <pilar.rus@schaman.com>
2020-06-04 16:11:07 +0200

    [Info] Added text

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,5 @@
 <template>
-    <span>Aqui va el contenido de Info</span>
+    <span>Este juego está compuesto por una cuadrícula de 9x9 casillas, dividida en regiones de 3x3 casillas. Partiendo de algunos números ya dispuestos en algunas de las casillas, hay que completar las casillas vacías con dígitos del 1 al 9 sin que se repitan por fila, columna o región.</span>
 </template>
 
 <script>
