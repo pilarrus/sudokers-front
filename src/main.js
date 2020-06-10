25ea519fc0b646f7524f220f0a828e29443c428d
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-06-10 18:18:19 +0200

    Install axios dependency

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "./axios";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -37,5 +38,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount("#app");
