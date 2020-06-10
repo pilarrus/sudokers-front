87e0aa62b883a48658bdaaaa05eef980efe973fd
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-06-10 21:59:36 +0200

    [NewOrResume] Added API request

--- src/views/NewOrResume.vue
+++ src/views/NewOrResume.vue
@@ -8,9 +8,11 @@
 </template>
 
 <script>
-  import mockFetch from "../utils/mockFetch";
+  // import mockFetch from "../utils/mockFetch";
   import ChooseNew from "../components/ChooseNew";
   import ChooseOpen from "../components/ChooseOpen";
+
+  const SERVER_ROUTE = process.env.VUE_APP_API_ROUTE;
 
   export default {
     name: "NewOrResume",
@@ -28,16 +30,34 @@
       this.sudokus = await this.setSudokus();
     },
     methods: {
-      loading: function (isLoading) {
+      setLoading: function (isLoading) {
         this.isLoading = isLoading;
       },
+      getSudokus: async function (userId, level) {
+        try {
+          const response = await this.axios({
+            method: 'get',
+            url: SERVER_ROUTE + '/sudokus/user/' + userId,
+            headers: {'Content-Type': 'application/json', 'token': this.$store.state.user.token}
+          });
+
+          if (response.status !== 200) {
+            return [];
+          }
+          const sudokus = response.data.data;
+          const sudokusByLevel = sudokus.filter(sudoku => sudoku.difficulty === level);
+          return sudokusByLevel;
+
+        } catch (e) {
+          return [];
+        }
+      },
       setSudokus: async function () {
-        const userId = this.$store.state.user._id;
+        const userId = this.$store.state.user.userId;
         const level = this.$store.state.level.name;
-        const sudokus = await mockFetch("/sudokus");
-        const sudokusByUser = sudokus.filter(sudoku => sudoku.user === userId && sudoku.difficulty === level);
-        this.loading(false);
-        return sudokusByUser;
+        const sudokus = await this.getSudokus(userId, level);
+        this.setLoading(false);
+        return sudokus;
       }
     }
   }
