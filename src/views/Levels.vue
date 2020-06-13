b67f99f1b841fefb52f2cd29dd0841db9ffb9b22
pilarrus <pilar.rus.izquierdo@gmail.com>
2020-06-13 15:22:01 +0200

    [Levels] Refactored code

--- src/views/Levels.vue
+++ src/views/Levels.vue
@@ -3,38 +3,38 @@
         <h2>Elige un nivel de dificultad</h2>
         <ul>
             <li v-for="level in levels" :key="level.text" v-on:click="actions(level)">
-                <Level v-bind:level="level.text" />
+                <Level :level="level.text" />
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-  import Level from "../components/Level.vue";
-  import { redirect } from "../utils/helpers";
+  import Level from '../components/Level.vue';
+  import { redirect } from '../utils/helpers';
 
   export default {
-    name: "Levels",
+    name: 'Levels',
     components: {
       Level
     },
     data() {
       return {
         levels: [
-          { name: "easy", text: "Fácil" },
-          { name: "medium", text: "Medio" },
-          { name: "difficult", text: "Dificíl" },
-          { name: "extreme", text: "Extremo" }
+          { name: 'easy', text: 'Fácil' },
+          { name: 'medium', text: 'Medio' },
+          { name: 'difficult', text: 'Dificíl' },
+          { name: 'extreme', text: 'Extremo' }
         ]
       }
     },
     methods: {
       setLevel: function (levelChoice) {
-        this.$store.commit("setLevel", levelChoice);
+        this.$store.commit('setLevel', levelChoice);
       },
       actions: function (levelChoice) {
         this.setLevel(levelChoice);
-        redirect("/newOrResume");
+        redirect('/newOrResume');
       }
     }
   }
